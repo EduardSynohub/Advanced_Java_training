c1bbd7a90962cf8c102c5381154332c76d47cb7d
eduard <eduard.synohub@gmail.com>
2026-01-29 23:48:58 +0100

    map/set tasks

---
+++ src/main/java/pl/eduard/training/collection/map_set/Task7.java
@@ -0,0 +1,31 @@
+package pl.eduard.training.collection.map_set;
+
+import pl.eduard.training.collection.list.Person;
+import pl.eduard.training.collection.list.Task5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Map<Integer, List<Person>> peopleByAge = new TreeMap<>();
+
+        for (Person p : Task5.filteredPeople()) {
+
+            if (!peopleByAge.containsKey(p.getAge())) {
+                List<Person> peopleWithSameAge = new ArrayList<>();
+                peopleWithSameAge.add(p);
+                peopleByAge.put(p.getAge(), peopleWithSameAge);
+            } else {
+                peopleByAge.get(p.getAge()).add(p);
+                peopleByAge.put(p.getAge(), peopleByAge.get(p.getAge()));
+            }
+        }
+
+        for (Map.Entry<Integer, List<Person>> entry : peopleByAge.entrySet()) {
+            System.out.println("People with age " + entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
