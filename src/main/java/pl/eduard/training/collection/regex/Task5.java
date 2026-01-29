4fbaddf404fbc525f50a3f43dbcee0214d2b163d
eduard <eduard.synohub@gmail.com>
2026-01-29 22:37:17 +0100

    regex tasks

---
+++ src/main/java/pl/eduard/training/collection/regex/Task5.java
@@ -0,0 +1,8 @@
+package pl.eduard.training.collection.regex;
+
+public class Task5 {
+    public static final String LOGIN_REGEX = "^[a-zA-Z_-][a-zA-Z0-9_-]{4,}$";
+    static boolean verifyLogin(String login) {
+        return login.matches(LOGIN_REGEX);
+    }
+}
